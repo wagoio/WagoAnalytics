1133a041fee31985a16d0ce5cb3c189384b9e063
QartemisT <QartemisT@gmail.com>
2021-05-03 15:31:31 +0100

    Init

---
+++ WTF.lua
@@ -0,0 +1,24 @@
+local _ = {
+	uuidXXXX = {
+		addonsLoaded = {
+			DBM = "1.0.5",
+		},
+		playerData = {
+			class = "WARRIOR",
+			specs = {"FURY", "ARMS"},
+			levelMin = 10,
+			levelMax = 12,
+			region = "US",
+		},
+		DBM = {
+			Counters = {
+				var1 = 5,
+				var2 = 10
+			},
+			Gauges = {
+				'var1',
+				'var2'
+			}
+		}
+	},
+}
